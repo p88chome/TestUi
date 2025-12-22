3a42895cf2643428d86130b23e5fb5e66c016b1d
Willy Chang <pppp2832@gmail.com>
2025-12-22 10:47:04 +0800

    改掉dashboard呈現、可以連通token計費

--- frontend/src/pages/DashboardPage.vue
+++ frontend/src/pages/DashboardPage.vue
@@ -153,7 +153,6 @@ import Button from 'primevue/button';
 import Dialog from 'primevue/dialog';
 import InputText from 'primevue/inputtext';
 import Textarea from 'primevue/textarea';
-import Textarea from 'primevue/textarea';
 // import { useToast } from 'primevue/usetoast';
 
 // const toast = useToast(); // Removed unused toast to fix build error 
